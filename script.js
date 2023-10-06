96b0b2fb40d31f2ff43f334a499fb3ae0fbb06fa
Reverson Barbosa <132270868+Reverson75@users.noreply.github.com>
2023-10-06 21:36:57 +0100

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-// cadastro cliente
 
-function main(){
-}
+// cadastro cliente
+ function main(){
+ 
  function registerClient(){
 
  function getdata(){
+     
+     const inputNome = document.getElementById("nome");
+     const valorNome = inputNome.value;
+     console.log(valorNome);
 
-    let nameClient = ""
-    let idClient = number
-    let payCard = ""
+ }
+ function checkdata(){
+     
+    let nameClient = "Reverson"
+    let idClient = 123.123.123
+    let payCard = true
 
         if(nameClient){
             let checkdata = (( nameClient = "")&&(idClient=== true)) 
@@ -18,16 +25,20 @@ function main(){
         }
         if(idClient){
             let checkId = (cardNumber === true )
-            console.log("idCheck!")
+            console.log("id Check!")
 
+        }else{
+            console.log("incorrect data!")
         }
-         
-}
- function checkdata(){
- 
-}
- function sendTosavedata(){
-
+         if(payCard){
+             let checkCard = ((payCard === true)&&(nameClient = ""))
+             console.log("payCard check")
+         }else{
+             console.log("incorrect data!")
+         }
+     
  }
-    
+ function sendTosavedata(){}
+ }
+
  }
